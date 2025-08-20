f64cecdcc456b0685ae669442e675e7dafe184f3
Juan <triky007@hotmail.com>
2025-08-20 12:03:03 +0200

    eliminacion de ficheros sobrantes despues de eliminar mobile

--- mobile/src/components/admin/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as AdminHeader } from './AdminHeader';
-export { default as CreateButton } from './CreateButton';
-export { default as ActionButtons } from './ActionButtons';
-export { default as AdminModal } from './AdminModal';
-export { default as FormField } from './FormField';
-export { default as FormPicker } from './FormPicker';
-export { default as RewardForm } from './RewardForm';
-export { default as TaskForm } from './TaskForm';
-export { default as UserForm } from './UserForm';
